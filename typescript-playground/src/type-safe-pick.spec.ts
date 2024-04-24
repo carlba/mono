60ed69af5a3c408ef88bffcf407d2eae4e446786
Carl Bäckström <genzorg@gmail.com>
2024-04-24 08:42:33 -0600

    feat: type-safe pick

---
+++ typescript-playground/src/type-safe-pick.spec.ts
@@ -0,0 +1,29 @@
+import { omit, pick } from './type-safe-pick';
+
+import { pick as lodashPick } from 'lodash';
+
+describe('type-safe-pick', () => {
+  it('should pick keys from object', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    const picked = lodashPick(obj, ['a', 'c', 'nisse']);
+
+    expect(picked).toEqual({ a: 1, c: 3 });
+  });
+
+  it('should omit keys from object', () => {
+    const obj = {
+      a: 1,
+      b: 2,
+      c: 3,
+    };
+
+    const omitted = omit(obj, ['b']);
+
+    expect(omitted).toEqual({ a: 1, c: 3 });
+  });
+});
--- typescript-playground/src/type-safe-pick.ts
+++ typescript-playground/src/type-safe-pick.ts
@@ -1,7 +1,3 @@
-export interface Mapper<T = any, R = any> {
-  (arg: T): R;
-}
-
 const isInArray = <T, A extends T>(item: T, array: ReadonlyArray<A>): item is A =>
   array.includes(item as A);
 
