0cbbcd095338a20431c9688e21dae238a4d69a00
Carl Bäckström <genzorg@gmail.com>
2024-04-25 22:03:24 -0600

    feat: add factories

---
+++ factories/src/index.ts
@@ -0,0 +1,24 @@
+import * as Factory from 'factory.ts';
+import { faker } from '@faker-js/faker';
+
+interface Person {
+  id: number;
+  firstName: string;
+  lastName: string;
+  fullName: string;
+  age: number;
+  parent_id: number;
+}
+
+const personFactory = Factory.Sync.makeFactory<Person>({
+  id: Factory.each(i => i),
+  firstName: 'Bob',
+  lastName: 'Smith',
+  fullName: 'Robert J. Smith, Jr.',
+  age: Factory.each(i => 20 + (i % 10)),
+  parent_id: Factory.each(i => faker.number.int({ min: 1, max: i - 1 })),
+});
+
+export function helloWorld() {
+  return 'Hello World!';
+}
