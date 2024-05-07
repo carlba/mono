ee02bf1434afee45fb0943eea01a424b545debec
Carl Bäckström <genzorg@gmail.com>
2024-05-07 09:26:50 -0600

    feat: add math project

---
+++ math/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Function that generates an array of fibonacci without recursion with 1
+ */
+
+function fibonacci(num: number): number {
+  if (num < 2) {
+    return num;
+  } else {
+    return fibonacci(num - 1)  fibonacci(num - 2);
+  }
+}
+
+console.log(fibonacci(10)); // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
