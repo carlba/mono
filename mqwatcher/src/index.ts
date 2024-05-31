1ce51d0afe5c78416723c7f9bfc15ff772dad99f
Carl Bäckström <genzorg@gmail.com>
2024-05-31 15:55:13 -0600

    feat: mqwatcher

--- mqwatcher/src/index.ts
+++ mqwatcher/src/index.ts
@@ -1,3 +1,22 @@
-export function helloWorld() {
-  return 'Hello World!';
-}
+import { Command } from '@commander-js/extra-typings';
+import * as amqplib from 'amqplib';
+
+const program = new Command()
+  .name('watch')
+  .description('Watch MQ')
+  .version('0.0.1')
+  .argument('<queue>', 'The queue to watch')
+  .option('-u, --uri <URI>', 'The AMPQ URI', 'amqp://localhost')
+  .action(async (queue, options) => {
+    const conn = await amqplib.connect(options.uri);
+    const channel = await conn.createChannel();
+    await channel.assertQueue(queue, { deadLetterExchange: 'dlx' });
+    await channel.bindQueue(queue, 'pocketlaw', '');
+    channel.consume(queue, msg => {
+      if (msg !== null) {
+        console.log(msg.content.toString());
+        channel.ack(msg);
+      }
+    });
+  })
+  .parse(process.argv);
